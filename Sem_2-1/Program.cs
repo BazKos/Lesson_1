81a263b6dd5e3b4783a892de4f7c9e3fe4ce84e5
Konstantin <konstbaz@gmail.com>
2023-12-24 11:40:47 +0300

    исправвил ошибку в Sem_2-1

--- Sem_2-1/Program.cs
+++ Sem_2-1/Program.cs
@@ -2,5 +2,5 @@
 //    трёхзначное число и удаляет вторую цифру этого числа.
 
 Console.WriteLine("Enter number");
-int a = int.Parse(Console.RedLine()!);
+int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine(a / 100 * 10 + a % 10);
