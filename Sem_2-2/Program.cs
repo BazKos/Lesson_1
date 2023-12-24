20ac646264afe8712f7934f7c1f4c680abde3f44
Konstantin <konstbaz@gmail.com>
2023-12-24 10:07:45 +0300

    2 задача 2 семинара

---
+++ Sem_2-2/Program.cs
@@ -0,0 +1,8 @@
+﻿// 2. Напишите программу, которая принимает на вход трёхзначное
+//    число и возводит вторую цифру этого числа в степень, равную
+//    третьей цифре.
+
+int a = int.Parse(Console.ReadLine()!);
+int secondNum = a / 10 % 10 ;
+int thirdNum = a % 10;
+Console.WriteLine(Math.Pow(secondNum,thirdNum));
