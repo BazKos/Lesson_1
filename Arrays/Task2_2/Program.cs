beb9e063ce0ec3ab11b61491b57acb86aec3c01e
Konstantin <konstbaz@gmail.com>
2024-01-12 23:44:59 +0300

    Лекция 2. Массивы. Выполнили Задачу 1

---
+++ Arrays/Task2_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Создать массив и заполнить его целыми числами
+//от 1 до N (N - произвольное натуральное число).
+//Вывести элементы на экран.
+
+Console.WriteLine("Введите натуральное число N:");
+int N = int.Parse(Console.ReadLine()!);
+int [] arr = new int[N];
+int i = 0;
+//while (i < N)
+while (i < arr.Length)
+{
+    arr [i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
