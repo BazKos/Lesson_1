0fa31863a23a751e2b5c79983abe0604387d9324
Konstantin <konstbaz@gmail.com>
2024-01-13 02:06:17 +0300

    Лекция 2. Массивы. Ознакомились с For и Foreach для задачи 3

--- Arrays/Task2_4/Program.cs
+++ Arrays/Task2_4/Program.cs
@@ -1,7 +1,7 @@
 ﻿//Дано N гирь с разным весом в случайном порядке.
 //Определить вес самой тяжелой гири.
 
-int [] arr = {16, 13, 8, 32, 4};
+int [] arr = {16, 13, 100, 32, 4};
 int i = 0;
 int max = 0;
 while (i < arr.Length)
@@ -13,3 +13,22 @@ while (i < arr.Length)
     i = i + 1;
 }
 Console.Write($"Самая тяжелая гиря имеет массу {max} кг");
+
+for (int j = 0; j < arr.Length; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+Console.Write($"Самая тяжелая гиря имеет массу {max} кг");
+
+max = arr[0];
+foreach (int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
