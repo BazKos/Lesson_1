6bf14c8a845c1cc492aaa430077eebee1bbd3964
Konstantin <konstbaz@gmail.com>
2024-01-13 21:09:53 +0300

    Лекция 3. Функции. Решили задачу 1.

---
+++ Functions/lecture3_1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Вычислить значение формулы(a*b)/(c+d),
+//где a, b, c, d - некоторые целые числа.
+//Решение оформить в виде функции.
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+double result = CalculateFormula(4, 6, 1, 5);
+Console.WriteLine(result);
