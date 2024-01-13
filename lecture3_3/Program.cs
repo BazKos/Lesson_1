16db1d63fbed293719936c83e4fae9e51c24de42
Konstantin <konstbaz@gmail.com>
2024-01-13 22:52:48 +0300

    Лекция 3. Функции. Решили задачу 3.

---
+++ lecture3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Создадим функцию обнуления четных элементов массива.
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = { 1, 3, 5, 7, 9, 11 };
+ZeroEvenElements(array);
+PrintArray(array);
